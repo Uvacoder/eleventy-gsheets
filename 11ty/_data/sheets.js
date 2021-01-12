dc0278f5b55a3014f6a19bbb08ff3f9af2ca8ea2
wommy <tom@technomad.media>
2021-01-11 22:19:08 -0500

    squash formatting

--- 11ty/_data/sheets.js
+++ 11ty/_data/sheets.js
@@ -5,7 +5,8 @@ module.exports = async function() {
 	let url = `https://spreadsheets.google.com/feeds/cells/${process.env.SHEETS_key}/1/public/full?alt=json`
 
 	return axios.get(url).then( 
-		res => res.data.feed.entry
-			.map( ea => ea.content["$t"] )
+		res => res.data.feed.entry.map( 
+			ea => ea.content["$t"]
+		)
 	) 
 }
